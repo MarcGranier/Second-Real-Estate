47d9b7236695554b779e933653e951038f726698
Marc Granier <mgranier@comcast.net>
2023-10-17 17:01:28 -0400

    Connecyed to MongDB

--- api/index.js
+++ api/index.js
@@ -1,7 +1,12 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from '../config/db.js';
+dotenv.config();
 
 const app = express();
 
-app.listen(3000, () => {
-	console.log(`Server is listening on port 3000`);
-});
+connectDB();
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
