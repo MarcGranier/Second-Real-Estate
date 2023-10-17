013e177d0183c41ed32f6dcbc788af303c5e6b61
Marc Granier <mgranier@comcast.net>
2023-10-17 17:42:09 -0400

    create a test API route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from '../config/db.js';
+import userRouter from './routes/user.route.js';
 dotenv.config();
 
 const app = express();
@@ -10,3 +11,5 @@ connectDB();
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
+
+app.use('/api/user', userRouter);
