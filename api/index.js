f3f63245726950f105980e8f244487374e3f0dce
Marc Granier <mgranier@comcast.net>
2023-10-18 14:14:37 -0400

    Create Signup process

--- api/index.js
+++ api/index.js
@@ -2,9 +2,13 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from '../config/db.js';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 dotenv.config();
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 connectDB();
 
@@ -13,3 +17,4 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
 
 app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
