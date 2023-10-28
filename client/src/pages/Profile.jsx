fd1aa6680a8812e485c0d57b73fc200f5c2bb6e7
Marc Granier <mgranier@comcast.net>
2023-10-28 16:38:04 -0400

    Update user functionality not working

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,4 +1,4 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { useRef, useState, useEffect } from 'react';
 import {
 	getStorage,
@@ -7,6 +7,11 @@ import {
 	uploadBytesResumable,
 } from 'firebase/storage';
 import { app } from '../firebase';
+import {
+	updateUserStart,
+	updateUserSuccess,
+	updateUserFailure,
+} from '../redux/user/userSlice';
 
 export default function Profile() {
 	const fileRef = useRef(null);
@@ -15,6 +20,7 @@ export default function Profile() {
 	const [filePerc, setFilePerc] = useState(0);
 	const [fileUploadError, setFileUploadError] = useState(false);
 	const [formData, setFormData] = useState({});
+	const dispatch = useDispatch();
 
 	//firebase storage
 	//allow read;
@@ -52,10 +58,37 @@ export default function Profile() {
 		);
 	};
 
+	const handleChange = (e) => {
+		setFormData({ ...formData, [e.target.id]: e.target.value });
+	};
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		try {
+			dispatch(updateUserStart());
+			const res = await fetch(`/api/user/update/${currentUser._id}`, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify(formData),
+			});
+			const data = await res.json();
+			if (data.success === false) {
+				dispatch(updateUserFailure(data.message));
+				return;
+			}
+
+			dispatch(updateUserSuccess(data));
+		} catch (error) {
+			dispatch(updateUserFailure(error.message));
+		}
+	};
+
 	return (
 		<div className='p-3 max-w-lg mx-auto'>
 			<h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
-			<form className='flex flex-col gap-4'>
+			<form onSubmit={handleSubmit} className='flex flex-col gap-4'>
 				<input
 					onChange={(e) => setFile(e.target.files[0])}
 					type='file'
@@ -65,7 +98,7 @@ export default function Profile() {
 				/>
 				<img
 					onClick={() => fileRef.current.click()}
-					src={formData.avatar || currentUser.avatar}
+					src={formData?.avatar || currentUser.avatar}
 					alt='profile'
 					className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2'
 				/>
@@ -85,18 +118,23 @@ export default function Profile() {
 				<input
 					type='text'
 					placeholder='username'
+					defaultValue={currentUser.username}
 					id='username'
 					className='border p-3 rounded-lg'
+					onChange={handleChange}
 				/>{' '}
 				<input
 					type='email'
 					placeholder='email'
+					defaultValue={currentUser.email}
 					id='email'
 					className='border p-3 rounded-lg'
+					onChange={handleChange}
 				/>{' '}
 				<input
 					type='text'
 					placeholder='password'
+					onChange={handleChange}
 					id='password'
 					className='border p-3 rounded-lg'
 				/>
