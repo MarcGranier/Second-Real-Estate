1447f78c862f2d2080f961ed9086a0a8646d81e6
Marc Granier <mgranier@comcast.net>
2023-11-09 15:05:16 -0500

    Delete user functionality

--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -25,11 +25,23 @@ const userSlice = createSlice({
 			state.loading = true;
 		},
 		updateUserSuccess: (state, action) => {
-			state.currentUser = action.payload;
+			state.currentUser = null;
 			state.loading = false;
 			state.error = null;
 		},
 		updateUserFailure: (state, action) => {
+			state.error = action.payload;
+			state.loading = false;
+		},
+		deleteUserStart: (state) => {
+			state.loading = true;
+		},
+		deleteUserSuccess: (state, action) => {
+			state.currentUser = null;
+			state.loading = false;
+			state.error = null;
+		},
+		deleteUserFailure: (state, action) => {
 			state.error = action.payload;
 			state.loading = false;
 		},
@@ -43,6 +55,9 @@ export const {
 	updateUserStart,
 	updateUserSuccess,
 	updateUserFailure,
+	deleteUserStart,
+	deleteUserSuccess,
+	deleteUserFailure,
 } = userSlice.actions;
 
 export default userSlice.reducer;
