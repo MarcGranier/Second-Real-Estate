7032bd84ca707f1c5a9c1749ce37e8a9ad05d242
Marc Granier <mgranier@comcast.net>
2023-11-10 14:23:37 -0500

    Sign Out functionality complete

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -14,7 +14,13 @@ import {
 	deleteUserStart,
 	deleteUserSuccess,
 	deleteUserFailure,
+	signInStart,
+	signInSuccess,
+	signOutUserFailure,
+	signOutUserStart,
+	signOutUserSuccess,
 } from '../redux/user/userSlice';
+import { deleteUser } from '../../../api/controllers/user.controller';
 
 export default function Profile() {
 	const fileRef = useRef(null);
@@ -107,6 +113,21 @@ export default function Profile() {
 		}
 	};
 
+	const handleSignOut = async () => {
+		try {
+			dispatch(signOutUserStart());
+			const res = await fetch('/api/auth/signout');
+			const data = await res.json();
+			if (data.success === false) {
+				dispatch(deleteUserFailure(data.message));
+				return;
+			}
+			dispatch(deleteUserSuccess(data));
+		} catch (error) {
+			dispatch(deleteUserFailure(error.message));
+		}
+	};
+
 	return (
 		<div className='p-3 max-w-lg mx-auto'>
 			<h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -164,9 +185,16 @@ export default function Profile() {
 					Update
 				</button>
 			</form>
-			<div onClick={handleDeleteUser} className='flex justify-between mt-5'>
-				<span className='text-red-700 cursor-pointer'>Delete Account</span>
-				<span className='text-red-700 cursor-pointer'>Sign out</span>
+			<div className='flex justify-between mt-5'>
+				<span
+					onClick={handleDeleteUser}
+					className='text-red-700 cursor-pointer'
+				>
+					Delete Account
+				</span>
+				<span onClick={handleSignOut} className='text-red-700 cursor-pointer'>
+					Sign out
+				</span>
 			</div>
 			<p className='text-red-700 mt-5'>{error ? error : ''}</p>
 			<p className='text-green-700 mt-5'>
