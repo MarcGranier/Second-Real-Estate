4880eeb41608123b0350ebe04c677ed9cc34328f
Marc <mgranier@comcast.net>
2023-11-21 17:04:11 -0500

    Add contact landlord to the listing page

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import SwiperCore from 'swiper';
+import { useSelector } from 'react-redux';
 import { Navigation } from 'swiper/modules';
 import 'swiper/css/bundle';
 import {
@@ -12,6 +13,7 @@ import {
 	FaParking,
 	FaChair,
 } from 'react-icons/fa';
+import Contact from '../components/Contact';
 
 export default function Listing() {
 	SwiperCore.use([Navigation]);
@@ -19,7 +21,9 @@ export default function Listing() {
 	const [loading, setLoading] = useState(false);
 	const [error, setError] = useState(false);
 	const [copied, setCopied] = useState(false);
+	const [contact, setContact] = useState(false);
 	const params = useParams();
+	const { currentUser } = useSelector((state) => state.user);
 
 	useEffect(() => {
 		const fetchListing = async () => {
@@ -109,7 +113,7 @@ export default function Listing() {
 							</span>
 							{listing.description}
 						</p>
-						<ul className='text-green-700 font-semibold text-sm flex flex-wrap items-center gap-4 sm: gap-6'>
+						<ul className='text-green-700 font-semibold text-sm flex flex-wrap items-center gap-4 sm:gap-6'>
 							<li className='flex items-center gap-1 whitespace-nowrap'>
 								<FaBed className='text-xl' />
 								{listing.bedrooms > 1
@@ -131,6 +135,15 @@ export default function Listing() {
 								{listing.furnished ? 'Furnished' : 'Unfurnished'}
 							</li>
 						</ul>
+						{currentUser && listing.userRef !== currentUser._id && !contact && (
+							<button
+								onClick={() => setContact(true)}
+								className='bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 p-3'
+							>
+								Contact landlord
+							</button>
+						)}
+						{contact && <Contact listing={listing} />}
 					</div>
 				</div>
 			)}
