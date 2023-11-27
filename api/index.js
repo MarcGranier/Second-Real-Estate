d853956801112e384a34859029311dbea1ff476d
Marc <mgranier@comcast.net>
2023-11-27 14:12:38 -0500

    Add pre installtion

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 dotenv.config();
 
 const app = express();
@@ -15,6 +16,8 @@ app.use(cookieParser());
 
 connectDB();
 
+const __dirname = path.resolve();
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
@@ -22,6 +25,12 @@ app.listen(port, () => console.log(`Server running on port ${port}`));
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
 
 app.use((err, req, res, next) => {
 	const statusCode = err.statusCode || 500;
